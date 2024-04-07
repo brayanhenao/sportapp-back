4f0c6f75e0816e71b0b0de4a00d005ade0a745cc
Brayan Henao <b.henaoc@uniandes.edu.co>
2024-04-07 01:47:32 -0400

    refactor: Remove unused code from experimentos (will be stored in a long-live branch as context) (#15)

--- projects/alerts/app/config/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-class Config:
-    ADVERSE_INCIDENTS_ALERTS_QUEUE = os.environ.get("ADVERSE_INCIDENTS_ALERTS_QUEUE", "adverse_incidents_queue.fifo")
-    NUTRITIONAL_PLAN_ALERTS_QUEUE = os.environ.get("NUTRITIONAL_PLAN_ALERTS_QUEUE", "nutritional_plan_queue.fifo")
